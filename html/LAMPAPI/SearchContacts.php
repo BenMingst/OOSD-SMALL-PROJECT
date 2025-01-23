8e13ccad10e46bcb097b0878e3b0360fa5e5936f
Matt-I03 <matthewidoni5@gmail.com>
2025-01-22 19:17:35 -0500

    stuf

--- html/LAMPAPI/SearchContacts.php
+++ html/LAMPAPI/SearchContacts.php
@@ -12,7 +12,7 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("SELECT FirstName, LastName, Phone, Email FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ?) AND UserID=?");
+		$stmt = $conn->prepare("SELECT FirstName, LastName, Phone, Email FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ?)");
 		$searchTerm = "%" . $inData["search"] . "%";
 		$stmt->bind_param("sss", $searchTerm, $searchTerm, $inData["userId"]);
 		$stmt->execute();

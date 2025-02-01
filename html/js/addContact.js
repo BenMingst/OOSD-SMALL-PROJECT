ecf434d104cf3371eaa832a949a86b29ce8a448d
Christian1137 <chris.y5@icloud.com>
2025-01-31 22:23:16 -0500

    added auto phone number format

--- html/js/addContact.js
+++ html/js/addContact.js
@@ -11,17 +11,29 @@ function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-document.getElementById('add-phone').addEventListener('input', function () {
-    const phoneInput = this.value;
-    const phoneError = document.getElementById('phone-error');
-
-    if (!validatePhoneNumber(phoneInput)) {
-        phoneError.textContent = 'Invalid phone number format. Use 123-456-7890.';
-        phoneError.style.display = 'block';
-    } else {
-        phoneError.style.display = 'none';
-    }
-});
+window.addEventListener('load', () => {
+    const phoneInput = document.querySelector('#add-phone');
+    phoneInput.addEventListener('keydown', disallowNonNumericInput);
+    phoneInput.addEventListener('keyup', formatToPhone);
+  });
+  
+const disallowNonNumericInput = (evt) => {
+    if (evt.ctrlKey) { return; }
+    if (evt.key.length > 1) { return; }
+    if (/[0-9.]/.test(evt.key)) { return; }
+    evt.preventDefault();
+}
+  
+const formatToPhone = (evt) => {
+    const digits = evt.target.value.replace(/\D/g,'').substring(0,10);
+    const areaCode = digits.substring(0,3);
+    const prefix = digits.substring(3,6);
+    const suffix = digits.substring(6,10);
+  
+    if(digits.length > 6) {evt.target.value = `(${areaCode}) ${prefix} - ${suffix}`;}
+    else if(digits.length > 3) {evt.target.value = `(${areaCode}) ${prefix}`;}
+    else if(digits.length > 0) {evt.target.value = `(${areaCode}`;}
+};
 
 document.getElementById('add-email').addEventListener('input', function () {
     const emailInput = this.value;
